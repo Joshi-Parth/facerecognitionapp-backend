af78b2eab2a03482f18fcc0d2dc02f714f1aff5e
Parth Joshi <joshiparth705@gmail.com>
2021-04-05 13:22:23 +0530

    fix

--- server.js
+++ server.js
@@ -3,21 +3,23 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const knex = require('knex');
-// const { response } = require('express');
+
 
 
 const register = require('./controllers/register')
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
-const image = require('./controllers/image')
+const image = require('./controllers/image');
 
  const db = knex({
     client: 'pg',
     connection: {
       connectionString : process.env.DATABASE_URL,
-      ssl: true,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
-  });
+ });
 
 
 const app = express();
